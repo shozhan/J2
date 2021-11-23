450cac5ef428cf3f895cbe54b7621ed21117fbf3
shozhan <87616592+shozhan@users.noreply.github.com>
2021-11-23 12:39:48 +0530

    New commit

--- ScheduleJob/src/job/Print.java
+++ ScheduleJob/src/job/Print.java
@@ -5,7 +5,7 @@ public class PrintConsole {
 	public static void main(String[] args) {
 		
 		System.out.println("Hurray, the second Jenkins Job 2");
-
+		System.out.println("Hurray, the second Jenkins Job 3");
 	}
 
 }
