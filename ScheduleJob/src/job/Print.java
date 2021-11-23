9b3999fc62e8576debd3bb0a89e9ecac21617b1c
shozhan <87616592+shozhan@users.noreply.github.com>
2021-11-23 11:30:52 +0530

    Updated Print file

--- ScheduleJob/src/job/Print.java
+++ ScheduleJob/src/job/Print.java
@@ -3,7 +3,7 @@ package job;
 public class PrintConsole {
 
 	public static void main(String[] args) {
-		
+		System.out.println("Hurray, the second Jenkins Job 2");
 		System.out.println("Hurray, the second Jenkins Job 2");
 
 	}
