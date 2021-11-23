193ed318437e123682097ca07f01c7c8048c9ff9
shozhan <87616592+shozhan@users.noreply.github.com>
2021-11-23 16:11:45 +0530

    UPdated Print File

--- ScheduleJob/src/job/Print.java
+++ ScheduleJob/src/job/Print.java
@@ -4,9 +4,7 @@ public class PrintConsole {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hurray, the second Jenkins Job 2");
-		System.out.println("Hurray, the second Jenkins Job 3");
-		System.out.println("Hurray, the second Jenkins Job 4");
+		System.out.println("Updated code");
 	}
 
 }
